0454433f09cc01db790215b0c4def746b1823db4
harrylo <lo.harry@gmail.com>
2018-07-28 23:29:48 +0800

    testing excel interfacing using openpyxl
    change the file locally.py.txt to locally.py
    add example.xlsx

--- create on github.py
+++ create on github.py
@@ -1,1 +1,20 @@
-this file is created using github
+import openpyxl
+import numpy
+
+wb_read = openpyxl.load_workbook('example.xlsx')
+sheet = wb_read['Sheet1']
+
+for i in range(1, 8, 2):
+    print(i, sheet.cell(row=i, column=2).value)
+
+#create a workbook
+wb_write = openpyxl.Workbook()
+
+#get the reference of an active sheet
+sheet_write = wb_write.active
+
+#set the title of the active sheet
+sheet_write.title = 'Spam Spam Spam'
+
+#create the workbook
+wb_write.save('example_copy.xlsx')
